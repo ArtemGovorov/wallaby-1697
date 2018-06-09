546da5ece05158187ee1827b278d018552fe98aa
Artem Govorov <artem.govorov@gmail.com>
2018-06-09 10:26:05 +1000

    Initial commit

---
+++ wallaby.js
@@ -0,0 +1,85 @@
+// Wallaby.js configuration
+
+var path = require('path');
+var webpack = require('webpack');
+var wallabyWebpack = require('wallaby-webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+var wallabyPostprocessor = wallabyWebpack({
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx", ".json"]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          use: [{
+            loader: "css-loader",
+            options: {
+              localIdentName: '[local]--[hash:5]',
+              sourceMap: true
+            }
+
+          }, {
+            loader: "less-loader",
+            options: {
+              sourceMap: true
+            }
+          }],
+          fallback: "style-loader",
+          publicPath: "../"
+        }),
+        exclude: "/node_modules/"
+      },
+      {
+        test: /\.html$/,
+        use: 'raw-loader'
+      },
+      {
+        test: /\.jpe?g$|\.gif$|\.png$/i,
+        loader: "file-loader?name=assets/img/[name].[ext]"
+      },
+      {
+        test: /\.woff2?$|\.ttf$|\.eot$|\.svg$/,
+        use: "file-loader?name=assets/[name].[ext]"
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('development')
+      }
+    }),
+    new webpack.NamedModulesPlugin()
+  ]
+});
+
+module.exports = function (wallaby) {
+  return {
+    files: [
+      { pattern: 'src/**/*.ts', load: false },
+    ],
+
+    tests: [
+      { pattern: 'tests/**/*Spec.ts', load: false }
+    ],
+
+    compilers: {
+      '**/*.ts?(x)': wallaby.compilers.typeScript({
+        "experimentalDecorators": true,
+        "emitDecoratorMetadata": true
+      })
+    },
+
+    postprocessor: wallabyPostprocessor,
+
+    setup: function () {
+      // required to trigger test loading
+      window.__moduleBundler.loadTests();
+    }
+  };
+};
